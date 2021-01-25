25ae574d269c0b3d340349bbf1962513ec41db09
Paul Marechal <paulmarechal@macbook-pro-de-paul.home>
2021-01-25 21:58:09 +0100

    button header

--- _includes/header.php
+++ _includes/header.php
@@ -6,13 +6,19 @@
          <span><a href=""><img src="./_assets/image/logoOK.png" width="150" height="150" alt="logo feeling food"></a></span>
       </div>
       <div>
-         <ul class="block-bouton"
+         <ul class="block-bouton">
             <li class="bouton"><a class="btn btn-sm btn-outline-secondary" href="index.php?page=signup">Sign Up</a></li>
             <li class="bouton"><a class="btn btn-sm btn-outline-secondary" href="index.php?page=login">Log In</a></li>
          </ul>
       </div>
    </div>
-
+   <div>
+      <div class="location-search">
+         <input class="adress-input" name="adress-input" type="text" placeholder="indiquez votre adresse">
+         <button class="submit-button" name="submit-button" type="submit">-></button>
+         <button class="geolocalisation" name="geolocalisation" type="submit">Trouver ma position</button>
+      </div>
+   </div>
 
 
  </header>
---
+++ views/produit_view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+	<html lang="fr">
+    <link rel="stylesheet" href="../_assets/style/styleDescription.css">
+    <body>
+        <div class="descriptionProduit">
+            <!-- Affiche le titre du produit -->
+            <header id="titreDescriptionProduit">
+                <h1>Recuperation du nom produit</h1>
+            </header>
+            <main>
+                <!-- Affiche les allergenes et le Kcal des differents produits -->
+                <section id="allergenesDescriptionProduits">
+                    <p>
+                        Récuperation des allergenes 
+                        Kcal 
+                    </p>
+                </section>
+                <section id="descriptionDescriptionProduit">
+                    <p>Récuperation de la description du produits (BDD)</p>
+                    <p>Affichage de la photo du producteur</p>
+                </section>
+                <section id="affichegeCarteDescriptionProduit">
+                    <p>Affichage de l'origine du produit (iframe google)</p>
+
+                </section>
+            </main>
+        </div>
+    </body>
