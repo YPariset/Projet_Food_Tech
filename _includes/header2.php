fc0579a7aaa8d1c7abdbf8cc43b7e2b847a50003
YPariset <yoann.pariset@edu.itescia.fr>
2021-01-26 00:17:50 +0100

    section home started

--- _includes/header2.php
+++ _includes/header2.php
@@ -1,19 +1,13 @@
 <header>
 <div id="header2">
       <div>
-         <span><a href=""><img src="./_assets/image/logoOK.png" width="40" height="40" alt="logo feeling food"></a></span>
+         <span><a href=""><img src="./_assets/image/logoOK.png" width="60" height="60" alt="logo feeling food"></a></span>
       </div>
-      
-
       <div class="location-search2">
-
-         <button class="geolocalisation2" name="geolocalisation" type="submit"><span><i class="fas fa-location-arrow"></i></span></button>
+         <button class="geolocalisation2" name="geolocalisation" type="submit"><span class="iconLoc"><i class="fas fa-location-arrow"></i></span></button>
          <input class="adress-input2" name="adress-input" type="text" placeholder="indiquez votre adresse">
          <button class="submit-button2" name="submit-button" type="submit"><span class="arrow"><i class="fas fa-arrow-right"></i></span></button>
-         
       </div>
-
-
       <div>
          <ul class="block-bouton2">
             <li class="bouton2 sign2"><a class="btnsign" href="index.php?page=signup">Sign Up</a></li>
--- views/home_view.php
+++ views/home_view.php
@@ -6,10 +6,37 @@
 </head>
 <body>
 <?php include_once '_includes/header.php'; ?> 
-
-<main >
-	
-
+<main>
+     <div class="section1">
+          <div class="text1">
+             <h2>Économiser sur chaque commande</h2>  
+               <p>blablabla</p>
+               <a class="btn btn-secondary" href="#">Essayez</a>
+          </div>
+          <div class="img1">
+               <img src="_assets/image/photo_bouffe.jpg" alt=""></img>
+          </div>
+     </div>
+     <div class="section2">
+          <div class="img2">
+               <img src="_assets/image/photo_bouffe.jpg" alt=""></img>
+          </div>
+          <div class="text2">
+          <h2>Économiser sur chaque commande</h2>  
+               <p>blablabla</p>
+               <a class="btn btn-secondary" href="#"></a>
+          </div>
+     </div>
+     <div class="section3">
+          <div class="text3">
+          <h2>Économiser sur chaque commande</h2>  
+               <p>blablabla</p>
+               <a class="btn btn-secondary" href="#"></a>
+          </div>
+          <div class="img3">
+               <img src="_assets/image/photo_bouffe.jpg" alt="" ></img>
+          </div>
+     </div>
 </main>
 
 </body>
