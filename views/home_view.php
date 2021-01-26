52b9d860ca662649d3e9c3d4ced814622ddf7851
seb830 <seb.flouvat@gmail.com>
2021-01-26 01:35:00 +0100

    apparition au scroll

--- views/home_view.php
+++ views/home_view.php
@@ -7,7 +7,7 @@
 <body>
 <?php include_once '_includes/header.php'; ?> 
 <main>
-     <div class="section1">
+     <div class="section1 appear">
           <div class="text1">
              <h2>Économiser sur chaque commande</h2>  
                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -20,7 +20,7 @@
                <img src="_assets/image/photo_bouffe.jpg" alt=""></img>
           </div>
      </div>
-     <div class="section2">
+     <div class="section2 appear">
           <div class="img2">
                <img src="_assets/image/imgVerticale.jpg" alt=""></img>
           </div>
@@ -33,7 +33,7 @@
                <a class="btn btn-secondary" href="#">Essayez</a>
           </div>
      </div>
-     <div class="section3">
+     <div class="section3 appear">
           <div class="text3">
           <h2>Économiser sur chaque commande</h2>  
                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -52,7 +52,29 @@
           </div>
      </div>
      
+     <script>
+          $(window).scroll(function(){
+               var scrolledFromTop = $(window).scrollTop() + $(window).height();
+                    $(".appear").each(function(){
+                         var distanceFromTop = $(this).offset().top;
+                              if(scrolledFromTop >= distanceFromTop+100){
+                                   console.log("hello");
+                                   var delaiAnim = $(this).data("delai");
+                                   $(this).delay(delaiAnim).animate({
+                                   top:0,
+                                   opacity:1
+          });
+    }
+  });
+});
+
+
+
+     </script>
 </main>
+
+
+
 
 </body>
 </footer>
