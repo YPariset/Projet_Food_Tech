7a2d3ee1322aef618870d970b8b8a406e890853b
mchemlal <michel.chemlal@gmail.com>
2021-01-26 23:44:11 +0100

    creation page login

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -1,34 +1,58 @@
 <?php
 
-    session_start();
-   
-    if(isset($_POST["username"]) && isset($_POST["password"])){
-        if (!empty($username) && !empty($password)){
-    //var_dump($resultat);
-            $username=htmlspecialchars($_POST["username"]);
-            $password=htmlspecialchars($_POST["password"]);
- 
-    // Comparaison du pass avec la bdd
-    $isPassCorrect = password_verify($_POST['password'], $resultat['password']);
-    $resultat = $db->prepare("SELECT * FROM Alimentation as A WHERE username= :username AND A.password = $isPassCorrect");
-    $resultat->bindparam("username", $username);
-    $resultat->bindparam("password", $password);  
 
-    //if(password_verify($pass, $))
 
-    if (!$resultat){
-        echo "L'identifiant ou le mot de passe est incorrect";
+ini_set('display_errors','on');
+error_reporting(E_ALL);
+session_start();
+
+
+
+global $db;
+
+
+$firstname = htmlspecialchars($_POST["firstname"]);
+$password = htmlspecialchars($_POST["password"]);
+var_dump($password);
+
+$req = $db->prepare('SELECT * FROM customer WHERE firstname = :firstname');
+$req->execute(array(
+    'firstname' => $firstname, 
+    
+));
+//var_dump($req);
+$resultat = $req->fetch();
+//$resultat = $req->rowCount();
+var_dump($resultat);
+
+
+//var_dump($resultat);
+
+// Comparaison du pass avec la bdd
+$isPassCorrect = password_verify($_POST['password'], $resultat['password']);
+var_dump($isPassCorrect);
+
+//if(password_verify($pass, $))
+
+if (!$resultat){
+    var_dump($resultat);
+    echo "L'identifiant ou le mot de passe est incorrect";
+} else {
+    if ($isPassCorrect){
+        //session_start();
+        $_SESSION['id'] = $resultat['id'];
+        $_SESSION['firstname'] = $resultat['firstname'];
+        $req->execute();
+        echo ("Vous etes co !");
     } else {
-        if ($isPassCorrect){
-            $_SESSION['id'] = $resultat['id'];
-            $_SESSION['username'] = $resultat['username'];
-            $req->execute();
-            header("Location:restaurant_view.php");
-        } else {
-            echo "L'identifiant ou le mot de passe est incorrect";
-        }
+        echo "L'identifiant ou le mot de passe est incorrect";
     }
 }
-}
+
+
+//header("Location:restaurant_view.php");
+
+
 
 ?>
+
